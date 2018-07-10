20f8b6d04c0bdc3eb531f1c90f4a7ab8db38f652
Samuel Nayerman <snayerman@lmtc-snayerman.jomax.paholdings.com>
2018-07-10 12:56:35 -0700

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-
+   validates :username, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: true
+   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
